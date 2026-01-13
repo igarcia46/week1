4270391fd10ec791d680f42bae58ee642e2d7016
Isaac <garciai0526@hotmail.com>
2026-01-13 10:45:06 -0500

    problem1 commit

---
+++ src/Conversion.java
@@ -0,0 +1,10 @@
+public class Conversion {
+
+    public static double footToMeter(double foot) {
+        return 0.305 * foot;
+    }
+
+    public static double meterToFoot(double meter) {
+        return meter / 0.305;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        // making sure table headers are aligned properly
+        System.out.printf("%-10s%-10s    %-10s%-10s%n",
+                "Feet", "Meters", "Meters", "Feet");
+        System.out.println("------------------------------------------");
+
+        // calculation
+        double meters = 20.0;
+
+        for (int feet = 1; feet <= 10; feet++) {
+            System.out.printf(
+                    "%-10.1f%-10.3f    %-10.1f%-10.3f%n",
+                    (double) feet,
+                    Conversion.footToMeter(feet),
+                    meters,
+                    Conversion.meterToFoot(meters)
+            );
+            meters += 5;
+        }
     }
 }
